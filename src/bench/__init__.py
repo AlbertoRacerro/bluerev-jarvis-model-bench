5fffb0f32f08ae1bf72dae22db4ad587d2cc8157
AlbertoRacerro <alberto.racerro@mail.polimi.it>
2026-07-10 10:35:47 +0200

    Initialize benchmark package

---
+++ src/bench/__init__.py
@@ -0,0 +1,5 @@
+"""BlueRev/Jarvis local-model benchmark package."""
+
+from .contracts import ContractError, extract_final, validate_manifest
+
+__all__ = ["ContractError", "extract_final", "validate_manifest"]
